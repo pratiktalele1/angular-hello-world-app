11f460a95b277f91bfd881ad900cd7eb652e9fcd
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-29 01:02:57 +0530

    valiadting to user name added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,18 @@ export class AppComponent {
   head='Hello from BridgeLabz';
   imgUrl='https://www.educationworld.in/wp-content/uploads/2020/06/BridgeLabz-2.jpg';
   userName;
+  nameError;
 
   showSite(){
     window.open('https://www.bridgelabz.com/');
+  }
+
+  onInput($event){
+    const regex=RegExp('^[A-Z]{1}[a-z]{3,}$');
+    if(regex.test($event.data)){
+      this.nameError="";
+    }else{
+      this.nameError="INVALID NAME";
+    }
   }
 }
