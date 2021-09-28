1e467c45e67de869cc40db3aa1e14f003b901e95
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-29 00:18:11 +0530

    BridgeLabz logo added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'welcome to hello World app';
   head='Hello from BridgeLabz';
+  imgUrl='https://www.educationworld.in/wp-content/uploads/2020/06/BridgeLabz-2.jpg';
 }
